85934408331366f839f6d919cc2444be3d73883b
Troy Novak <novak_troy@yahoo.com>
2018-02-20 17:57:21 -0600

    testing to see if this is working properly with my edition of Eclipse

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -6,7 +6,8 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
-		// just testing this out
+		// testing Git Repository
+		System.out.println("is this working");
 	}
 
 }
