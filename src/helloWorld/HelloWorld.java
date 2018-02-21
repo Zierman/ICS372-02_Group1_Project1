694463d2f9d8afcfbe2b077b3050396ea13cc986
Joshua Zierman <py1422xs@metrostate.edu>
2018-02-20 19:26:20 -0600

    removed extra output

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -6,12 +6,6 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
-<<<<<<< HEAD
-		System.out.println("Hello World2");
-=======
-		// testing Git Repository
-		System.out.println("is this working");
->>>>>>> branch 'master' of https://github.com/Zierman/ICS372-02_Group1_Project1.git
 	}
 
 }
