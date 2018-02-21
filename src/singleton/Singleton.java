34f5606dc41e0cfd5909cc0547aa16eef32dedac
Joshua Zierman <py1422xs@metrostate.edu>
2018-02-20 22:54:56 -0600

    Singleton Interface Changes
    -Removed the instance() method because it needs to be static.
    +Changed to use generices.

--- src/singleton/Singleton.java
+++ src/singleton/Singleton.java
@@ -7,8 +7,7 @@ package singleton;
  * @author Joshua Zierman [py1422xs@metrostate.edu]
  *
  */
-public interface Singleton
+public interface Singleton<Type>
 {
-	public Object instance();
-	public Object readResolve();
+	public Type readResolve();
 }
