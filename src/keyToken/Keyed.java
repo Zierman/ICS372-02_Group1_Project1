209709123e1b688e5e85c14696f1712955335289
Joshua Zierman <py1422xs@metrostate.edu>
2018-02-24 00:38:01 -0600

    documentation work

--- src/keyToken/Keyed.java
+++ src/keyToken/Keyed.java
@@ -1,11 +1,22 @@
 package keyToken;
 
+/**
+ * An interface that allows an object to be checked to see if a key value
+ * matches the object's key value
+ * 
+ * @author Joshua Zierman [py1422xs@metrostate.edu]
+ *
+ * @param <Key>
+ *            The datatype of the key value
+ */
 public interface Keyed<Key>
 {
 	/**
 	 * Checks if the object has a key that matches
-	 * @param key a key value
-	 * @return true if they match or false if they do not. 
+	 * 
+	 * @param key
+	 *            a key value
+	 * @return true if they match or false if they do not.
 	 */
 	public boolean matches(Key key);
 }
