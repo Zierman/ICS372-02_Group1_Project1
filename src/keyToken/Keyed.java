2900af0f4c645c48b667b2506acffa5f4ab610f3
Joshua Zierman <py1422xs@metrostate.edu>
2018-02-24 00:36:37 -0600

    more documentation work

--- src/keyToken/Keyed.java
+++ src/keyToken/Keyed.java
@@ -1,7 +1,11 @@
 package keyToken;
 
-//TODO document all of this
 public interface Keyed<Key>
 {
+	/**
+	 * Checks if the object has a key that matches
+	 * @param key a key value
+	 * @return true if they match or false if they do not. 
+	 */
 	public boolean matches(Key key);
 }
