14714e5ff75bfbd01f64019c6f6cd38af9e7d80b
Joshua Zierman <py1422xs@metrostate.edu>
2018-02-25 02:29:06 -0600

    docs

--- src/keyToken/KeyToken.java
+++ src/keyToken/KeyToken.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 //TODO document all of this
 /**
+ * A Key value token used to store a key such as an ID number
  * @author Joshua Zierman [py1422xs@metrostate.edu]
  *
  * @param <Type> They type of object this key is used for
@@ -11,29 +12,56 @@ import java.io.Serializable;
 public abstract class KeyToken <Type, Key> implements Serializable
 {
 	/**
-	 * 
+	 * Serilization version
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * The actual key value 
+	 */
 	private Key key;
 	
+	/**
+	 * Constructs a new Key Value
+	 */
 	public KeyToken()
 	{
 		key = getNextKey();
 		setLastKey(key);
 	}
 	
+	/**
+	 * Gets the next key to be generated.
+	 * @return a key value
+	 */
 	protected abstract Key getNextKey();
 	
+	/**
+	 * Gets the last key generated
+	 * @return a key value
+	 */
 	protected abstract Key getLastKey();
 	
+	/**
+	 * Sets the last key generated
+	 * @param key a key value
+	 */
 	protected abstract void setLastKey(Key key);
 	
+	/**
+	 * Sets the key 
+	 * @param key a key value
+	 */
 	protected void setKey(Key key)
 	{
 		this.key = key;
 	}
 	
+	/**
+	 * Checks if a key value matches this key value
+	 * @param key a key value
+	 * @return true if they match, else false
+	 */
 	public boolean matches(Key key)
 	{
 		return this.key.equals(key);
