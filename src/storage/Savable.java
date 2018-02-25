f2a706ab6fc461807ca316f5f7458d6ee20d9fda
Joshua Zierman <py1422xs@metrostate.edu>
2018-02-25 02:45:44 -0600

    docs

--- src/storage/Savable.java
+++ src/storage/Savable.java
@@ -5,12 +5,15 @@ package storage;
 
 import java.io.IOException;
 
-//TODO document all of this
 /**
+ * An interface for objects that can be written to storage with a save method call.
  * @author Joshua Zierman [py1422xs@metrostate.edu]
  *
  */
 public interface Savable
 {
+	/** Saves an object to a file.
+	 * @throws IOException if there is a problem with FileIO.
+	 */
 	public void save() throws IOException;
 }
