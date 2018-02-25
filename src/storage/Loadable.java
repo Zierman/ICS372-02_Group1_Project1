13f52bab0b3bf06321d9202be90fdaf164e105e9
Joshua Zierman <py1422xs@metrostate.edu>
2018-02-25 02:43:49 -0600

    docs

--- src/storage/Loadable.java
+++ src/storage/Loadable.java
@@ -5,12 +5,15 @@ package storage;
 
 import java.io.IOException;
 
-//TODO document all of this
-/**
+/** Interface for objects that can be read from storage with a load method call.
  * @author Joshua Zierman [py1422xs@metrostate.edu]
  *
  */
 public interface Loadable
 {
+	/** load the object from storage
+	 * @throws ClassNotFoundException if there is a problem with a class not being found
+	 * @throws IOException If there is an issue with the FileIO.
+	 */
 	public void load() throws ClassNotFoundException, IOException;
 }
