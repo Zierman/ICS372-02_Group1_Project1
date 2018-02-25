54b3bc5ff65aeff559bd4bdeced4e250db1e34ae
Joshua Zierman <py1422xs@metrostate.edu>
2018-02-25 02:40:44 -0600

    docs

--- src/singleton/Singleton.java
+++ src/singleton/Singleton.java
@@ -1,6 +1,5 @@
 package singleton;
 
-//TODO document all of this
 /**
  * An interface to force the use of {@link singleton.Singleton#readResolve()} when creating a singleton class. Unfortunately, simply implementing this does not functionally make it a singleton.
  * @author Joshua Zierman [py1422xs@metrostate.edu]
