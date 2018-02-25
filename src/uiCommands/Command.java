d62eaa1240825cfee7783a8a522c6071a4b2be29
Joshua Zierman <py1422xs@metrostate.edu>
2018-02-25 04:15:58 -0600

    Docs

--- src/uiCommands/Command.java
+++ src/uiCommands/Command.java
@@ -5,18 +5,35 @@ package uiCommands;
 
 import singleton.Singleton;
 
-//TODO document all of this
+
 /**
+ * Interface for Commands
  * @author Joshua Zierman [py1422xs@metrostate.edu]
  *
+ * @param <Type> the type of User Interface that the commands are designed for.
  */
 public interface Command<Type> extends Singleton<Command<Type>>
 {
+	/**
+	 * Gets a label to show to user describing the command.
+	 * @return the label.
+	 */
 	public String getLabel();
 
-	public void call(Type arg);
+	/**Executes the action associated with the command.
+	 * @param ui a user interface
+	 */
+	public void call(Type ui);
 
+	/**
+	 * Checks to see if the command accesses data.
+	 * @return true if the command accesses data, else false.
+	 */
 	public boolean isDataCommand();
 
+	/**
+	 * Checks to see if the command terminates the application.
+	 * @return true if the command is supposed to close the application.
+	 */
 	public boolean isTerminationCommand();
 }
