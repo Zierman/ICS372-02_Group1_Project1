c41d36795d6d6d0ae5749cdd231d482979359372
Joshua Zierman <py1422xs@metrostate.edu>
2018-02-25 02:33:25 -0600

    docs

--- src/ownership/Owned.java
+++ src/ownership/Owned.java
@@ -1,6 +1,15 @@
 package ownership;
 
+/**
+ * An interface for an owned object allowing a method to return an owner.
+ * @author Joshua Zierman [py1422xs@metrostate.edu]
+ *
+ * @param <Type> the type of the owner
+ */
 public interface Owned<Type>
 {
+	/**Gets the owner.
+	 * @return the owner object
+	 */
 	public Type getOwner();
 }
