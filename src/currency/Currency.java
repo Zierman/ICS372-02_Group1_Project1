a6f54467f070ea429ba0bc5e707119115fcb4e9c
Joshua Zierman <py1422xs@metrostate.edu>
2018-03-01 02:52:11 -0600

    Removed artifact from something I tried.
    I gave up on trying to create a generic parseable interface because of
    the static nature of the operation.

--- src/currency/Currency.java
+++ src/currency/Currency.java
@@ -2,7 +2,6 @@ package currency;
 
 import java.io.Serializable;
 
-import misc.Parsable;
 
 /**
  * An abstract class that is used when creating other types of currency.
@@ -12,7 +11,7 @@ import misc.Parsable;
  * @param <Type>
  *            they type of number that is used to store the currency value
  */
-public abstract class Currency<Type extends Number> implements Serializable, Parsable<Type>
+public abstract class Currency<Type extends Number> implements Serializable
 {
 	/**
 	 * the version of the serialization.
